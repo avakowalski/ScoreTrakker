801bb4f4998a78c37d5eac74ea7424a3ad28c713
Ava Kowalski <65259771+avakowalski@users.noreply.github.com>
2023-09-26 14:35:35 -0600

    fixed all the bugs by ava with the help of TA michaelgit add .

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,54 +1,69 @@
-import java.io.BufferedReader;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class ScoreTrakker {
+	//array that holds information about students 
     private ArrayList<Student> students;
+    //added new array to hold files 
+    private String[] files = {"scores.txt", "badscore.txt"," nofile.txt"};
 
-    public ScoreTrakker() {
-        students = new ArrayList<>();
-    }
+  
+    private void loadDataFile(String fileName) throws FileNotFoundException {
+		// !! Avas comment: The different way sadly had to be changed in part 2 
+    		//students.clear();
+    		FileReader reader = new FileReader(fileName);
+			Scanner doesit = new Scanner(reader);
+			students = new ArrayList<Student>();
+			    while (doesit.hasNextLine()) {
+			    	String name = doesit.nextLine();
+		            String score = doesit.nextLine();
+			    	//try to upload the users data from the file 
+			    	try {
+			    
+		            	//extracting the integer value 
+		                int score2 = Integer.valueOf(score);
+		                Student student = new Student(name, score2);
+		                students.add(student);
 
-    private void loadDataFile(String fileName) {
-    	// https://www.geeksforgeeks.org/java-io-bufferedreader-class-java/
-    	// https://www.youtube.com/watch?v=waXvGUEjTTs
-    	// https://www.youtube.com/watch?v=JaiCrtNMfEc
-    	// I had free time this weekend so I wanted to try it a different way than it was taught in class :)
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-            String text;
-            while ((text = reader.readLine()) != null) {
-                String name = text;
-                if ((text = reader.readLine()) != null) {
-                    int score = Integer.parseInt(text);
-                    Student student = new Student(name, score);
-                    students.add(student);
-                } else {
-                    System.err.println("Error: Incomplete data for student '" + name + "'");
-                }
-            }
-        } catch (IOException e) {
-            System.err.println("Error reading file: " + e.getMessage());
-        }
-    }
+			            }
+			    	//if its not a number then catch 
+			        catch (NumberFormatException e) {
+			        System.err.println("Incorrect format for " + name + " not a valid score: "+score);
+			        }
+			    }
+			    doesit.close();
+		}
 
     private void printInOrder() {
+    	System.out.println("Student Score List:");
     	// refrenced https://www.youtube.com/watch?v=rH0winlka8A
     	// and https://www.geeksforgeeks.org/collections-in-java-2/
+//        Collections.sort(students);
         Collections.sort(students);
-        System.out.println("Student Score List");
         for (Student student : students) {
             System.out.println(student);
         }
+        System.out.println();
     }
 
-    private void processFiles() {
-        loadDataFile("scores.txt");
-        printInOrder();
-    }
+    private void processFiles(){
+    	for (String i: files) {
+        	try {
+        		loadDataFile(i);
+        		printInOrder();
+        		
+        	} catch (FileNotFoundException e) {
+        	System.out.println("Cannot open file:" +i);
+        	System.out.println();
+        	}
+    	}
+       
+   }
 
-    public static void main(String[] args) {
+	public static void main(String[] args){
         ScoreTrakker scoreTrakker = new ScoreTrakker();
         scoreTrakker.processFiles();
     }
